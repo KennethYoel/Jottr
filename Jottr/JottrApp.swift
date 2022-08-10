97445b23240b0da22513eebb9dbaa3cae4cab0e6
Ken <kenneth.joel@live.com>
2022-08-10 12:22:44 +0200

    Initial Commit

---
+++ Jottr/JottrApp.swift
@@ -0,0 +1,17 @@
+//
+//  JottrApp.swift
+//  Jottr
+//
+//  Created by Kenneth Gutierrez on 8/10/22.
+//
+
+import SwiftUI
+
+@main
+struct JottrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
