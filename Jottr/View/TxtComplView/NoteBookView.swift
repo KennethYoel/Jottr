2a0b9121f6f3d6da536175a9a1b3d95502863d49
Ken <kenneth.joel@live.com>
2022-10-12 17:28:20 +0200

    Added NoteBookView

---
+++ Jottr/View/TxtComplView/NoteBookView.swift
@@ -0,0 +1,20 @@
+//
+//  NoteBookView.swift
+//  Jottr
+//
+//  Created by Kenneth Gutierrez on 10/12/22.
+//
+
+import SwiftUI
+
+struct NoteBookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NoteBookView_Previews: PreviewProvider {
+    static var previews: some View {
+        NoteBookView()
+    }
+}
